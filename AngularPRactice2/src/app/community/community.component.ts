2f7b2fb7398bf6543464fb0e4f132c2fd6ae3400
Shawn Yu <shawnyudesign@gmail.com>
2024-01-29 19:48:54 -0500

    moving community eleents

--- AngularPRactice2/src/app/community/community.component.ts
+++ AngularPRactice2/src/app/community/community.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef,OnChanges, Component } from '@angular/core';
 import { Router} from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { HomeService } from '../home.service';
@@ -25,6 +25,10 @@ export class CommunityComponent {
   currentUser: any = {};
   filter: string = '';
 
+  public itemPositions:any ={x:12, y:200}
+
+  public userItemPositions: any[] = [];
+
   getUserItems(userId: number) {
     console.log("getting the user's items");
     console.log(userId);
@@ -46,11 +50,43 @@ export class CommunityComponent {
         next: (items) => {
           console.log(items);
           this.userItems = items;
+          for (let i = 0; i < items.length; i++) {
+            this.userItemPositions.push([0, 0]);
+          }
         },
       });
   }
 
+  public selectedCard: number | null = null;
+
+  selectCard(index: number) {
+    console.log("clicked card")
+    console.log(index)
+    this.selectedCard = index;
+  }
+
+  moveCard(event: MouseEvent) {
+     this.changeDetectorRef.detectChanges();
+    console.log("movingcard")
+    console.log(this.selectedCard)
+    const mouseX = event.offsetX;
+    const mouseY = event.offsetY;
+    console.log(mouseX)
+    console.log(mouseY)
+if(this.selectedCard){
+     this.itemPositions.x= mouseX;
+     this.itemPositions.y = mouseY;
+    // this.userItemPositions[this.selectedCard][0] = mouseX;
+    // this.userItemPositions[this.selectedCard][1] = mouseY;
+    console.log(this.userItemPositions[this.selectedCard]);
+
+}
+
+    // Now you can use the mouseX and mouseY variables as you desire.
+  }
+
   constructor(
+    private changeDetectorRef: ChangeDetectorRef,
     private route: ActivatedRoute,
     private homeSV: HomeService,
     private http: HttpClient
